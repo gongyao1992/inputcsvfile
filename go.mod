639d1172667d172d792cd9ef871bf828eb8d5a39
gongyao <gongyao1992@qq.com>
2021-05-13 18:18:12 +0800

    [提交代码]

---
+++ go.mod
@@ -0,0 +1,5 @@
+module inputcsvfile
+
+go 1.16
+
+require github.com/gongyao1992/moddemo v0.0.0-20210512020319-9cbda6c78459
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gongyao1992/moddemo"
+)
+
+func main()  {
+
+	fmt.Println(moddemo.SayHi("aaa"))
+}
